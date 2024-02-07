66c9d277ebaeaa86b691948f364f1a4a80309ddf
alexsc1998 <alex.sawicki1998@gmail.com>
2024-02-07 06:32:16 -0500

    Update: cors

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import winston, { add } from 'winston';
-import { logger } from './logger.js';
-import { errorHandler, notFound } from './middleware/error-middleware.js';
-import { userRouter } from './users/route.js';
-import { userGroupRouter } from './userGroup/route.js';
-import { roleRouter } from './role/route.js';
-import { transactionLogRouter } from './transaction-limit/route.js';
-import { passwordHistoryRouter } from './password-history/route.js';
-import { maintenanceLogRouter } from './system-maintenance/route.js';
-import dotenv from 'dotenv';
-import { commonRouter } from './common/route.js';
-import { ENVOBJ } from './utils/common-types.js';
-import { ZodError } from 'zod';
-import { Role } from './db/schema.js';
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import winston, { add } from "winston";
+import { logger } from "./logger.js";
+import { errorHandler, notFound } from "./middleware/error-middleware.js";
+import { userRouter } from "./users/route.js";
+import { userGroupRouter } from "./userGroup/route.js";
+import { roleRouter } from "./role/route.js";
+import { transactionLogRouter } from "./transaction-limit/route.js";
+import { passwordHistoryRouter } from "./password-history/route.js";
+import { maintenanceLogRouter } from "./system-maintenance/route.js";
+import dotenv from "dotenv";
+import { commonRouter } from "./common/route.js";
+import { ENVOBJ } from "./utils/common-types.js";
+import { ZodError } from "zod";
+import { Role } from "./db/schema.js";
 
 declare global {
   namespace Express {
@@ -43,39 +43,40 @@ try {
   throw new Error((error as ZodError).message);
 }
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
     })
   );
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 } else {
   app.use(
-    morgan('common', {
-      stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {
-        flags: 'a',
+    morgan("common", {
+      stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
+        flags: "a",
       }),
     })
   );
 }
 
 const domains = [
-  'http://54.254.130.92:3000',
-  'http://54.254.130.92:3001',
-  'http://13.229.106.122:3000',
-  '196.191.190.114',
-  'http://196.191.190.114:3000',
-  'https://payment.bkrm.pro',
-  'https://admin.bkrm.pro',
-  'http://127.0.0.1:3000',
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://127.0.0.1:3001',
-  '127.0.0.1',
+  "http://54.254.130.92:3000",
+  "http://54.254.130.92:3001",
+  "http://13.229.106.122:3000",
+  "http://20.205.146.121:3001",
+  "196.191.190.114",
+  "http://196.191.190.114:3000",
+  "https://payment.bkrm.pro",
+  "https://admin.bkrm.pro",
+  "http://127.0.0.1:3000",
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://127.0.0.1:3001",
+  "127.0.0.1",
 ];
 const domainsToUse =
-  process.env.NODE_ENV !== 'production'
+  process.env.NODE_ENV !== "production"
     ? domains
     : domains.slice(0, domains.length - 5);
 
@@ -88,7 +89,7 @@ app.use(
       } else {
         return callback(
           new Error(
-            'The CORS policy for this site does not allow access from the specified Origin.'
+            "The CORS policy for this site does not allow access from the specified Origin."
           ),
           false
         );
@@ -116,17 +117,17 @@ app.use(express.json());
 //   next();
 // });
 
-app.get('/', async (req, res) => {
-  res.json({ message: 'hello' });
+app.get("/", async (req, res) => {
+  res.json({ message: "hello" });
 });
 
-app.use('/api/common', commonRouter);
-app.use('/api/users', userRouter);
-app.use('/api/groups', userGroupRouter);
-app.use('/api/roles', roleRouter);
-app.use('/api/transactions', transactionLogRouter);
-app.use('/api/maintenance', maintenanceLogRouter);
-app.use('/api/password-histories', passwordHistoryRouter);
+app.use("/api/common", commonRouter);
+app.use("/api/users", userRouter);
+app.use("/api/groups", userGroupRouter);
+app.use("/api/roles", roleRouter);
+app.use("/api/transactions", transactionLogRouter);
+app.use("/api/maintenance", maintenanceLogRouter);
+app.use("/api/password-histories", passwordHistoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
