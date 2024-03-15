091cda626502e1e55c3adad77f6df3d591363baf
Anjar Feri <anjarvery@gmail.com>
2024-03-16 02:20:12 +0700

    ok

--- src/system-maintenance/service.ts
+++ src/system-maintenance/service.ts
@@ -66,8 +66,8 @@ export async function getMntLogs(paymentSite: boolean) {
         return {
           ...item,
           mid: index + 1,
-          iRakyatStatus: item.iRakyatYN && item.approvedBy !='' && item.rejectReason =='' ? 'C' : '',
-          iBizRakyatStatus: item.iBizRakyatYN && item.approvedBy !='' && item.rejectReason =='' ? 'C' : '',
+          iRakyatStatus: item.iRakyatYN && item.approvedBy !='' ? 'C' : '',
+          iBizRakyatStatus: item.iBizRakyatYN && item.approvedBy !='' ? 'C' : '',
         };
       } else if (
         item.approvalStatus == 'Rejected' &&
