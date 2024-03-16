446d9286340ed2ff4517e5f61e4bc2c88b0f4968
Anjar Feri <anjarvery@gmail.com>
2024-03-16 21:14:04 +0700

    test

--- src/system-maintenance/service.ts
+++ src/system-maintenance/service.ts
@@ -67,8 +67,8 @@ export async function getMntLogs(paymentSite: boolean) {
         return {
           ...item,
           mid: index + 1,
-          iRakyatStatus: item.iRakyatYN && item.approvedBy !='' ? item.submissionStatus=='New' && item.approvalStatus=='Rejected' ?'':'C':'',
-          iBizRakyatStatus: item.iBizRakyatYN && item.approvedBy !='' ? item.submissionStatus=='New' && item.approvalStatus=='Rejected' ?'':'C':'',
+          iRakyatStatus: item.iRakyatYN && item.approvedBy !='' ? item.submissionStatus=='New' && item.approvalStatus=='Rejected' ?'':item.iRakyatStatus=='A' && item.submissionStatus=='Edited' && item.approvalStatus=='Rejected'?'C':'':'',
+          iBizRakyatStatus: item.iBizRakyatYN && item.approvedBy !='' ? item.submissionStatus=='New' && item.approvalStatus=='Rejected' ?'':item.iBizRakyatStatus=='A' && item.submissionStatus=='Edited' && item.approvalStatus=='Rejected'?'C':'':'',
         };
       } else if (
         item.approvalStatus == 'Rejected' &&
